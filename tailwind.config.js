e4067dcdf6ef495ec971cea257d038234d4487cb
Chelsea Joy de Guzman <chelsea.deguzman@mytikasllc.com>
2023-01-13 12:43:38 +0400

    main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 module.exports = {
-  purge: [],
+  content: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
-  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: '#555562',
+          200: '#4C4C57',
+          300: '#42424C',
+          400: '#393941',
+          500: '#2D2D34',
+          600: '#26262C',
+          700: '#1C1C21',
+          800: '#131316',
+          900: '#09090B'
+        },
+        accent: {
+          100: '#FFE5EC',
+          200: '#FFC2D1',
+          300: '#FFB3C6',
+          400: '#FF8FAB',
+          500: '#FA3869',
+          600: '#F9245A',
+          700: '#F9104A',
+          800: '#EF0640',
+          900: '#DB063B'
+        }
+      }
+    },
   },
   variants: {
     extend: {},
